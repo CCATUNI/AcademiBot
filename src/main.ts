d57f113f79e3a3a6440f27bc1885814d6fce4a54
Marco Vela <mvelar@uni.pe>
2020-11-09 02:01:52 -0500

    SELF PING

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import appConfig from './config/app.config';
 import { ConfigType } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan';
+import * as rp from 'request-promise';
+
+async function infinitePing(url: string) {
+  for (;;) {
+    let time = (Math.random() * 60 * 1000) + 1000*15;
+    await rp(url);
+    await new Promise(resolve => setTimeout(resolve, time));
+  }
+}
+
 
 // Developed by Marco Antonio Vela Rodriguez
 // mvelar@uni.pe
@@ -17,5 +27,8 @@ async function bootstrap() {
   app.use(morgan("tiny"));
   await app.listen(configService.port);
   console.log(`Running in ${await app.getUrl()} and listening at port ${configService.port}`);
+  if (configService.server)
+    infinitePing(configService.server)
+      .catch(console.error);
 }
 bootstrap();
